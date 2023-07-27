02d91af893de3eb71f305525c594b35ada6f7527
Robert Akhiyarov <57063378+rbrta@users.noreply.github.com>
2023-07-27 11:54:08 +0300

    feat: add figma/fontWeight values support for typography transformer (#13)
    * feat: add figma/fontWeight values support for typography transformer
    
    * feat: add all figma/fontWeight options

--- transforms/TypographyTransform.js
+++ transforms/TypographyTransform.js
@@ -7,6 +7,27 @@ const toKebab = (str) => StyleDictionary.transform['name/cti/kebab'].transformer
 
 const isTypographyToken = (token) => token.type === 'typography';
 
+const typoographyFontWeightDict = {
+  'Thin': '100', 
+  'Thin Italic': '100', 
+  'Extra Light': '200',
+  'Extra Light Italic': '200',
+  'Light': '300',
+  'Light Italic': '300',
+  'Regular': '400',
+  'Italic': '400',
+  'Medium': '500',
+  'Medium Italic': '500',
+  'Semi Bold': '600',
+  'Semi Bold Italic': '600',
+  'Bold': '700',
+  'Bold Italic': '700',
+  'Extra Bold': '800',
+  'Extra Bold Italic': '800',
+  'Black': '900',
+  'Black Italic': '900',
+}
+
 const typographyTransformer = ({ value, name }) => {
   if (!value) return;
 
@@ -14,8 +35,13 @@ const typographyTransformer = ({ value, name }) => {
 
   const stringValue = entries.map(([key, value], idx) => {
     const separator = idx === entries.length - 1 ? '' : ';';
+    let transformedValue = value
 
-    return `$${name}-${toKebab(key)}: ${value}${separator}`
+    if (key === 'fontWeight') {
+      transformedValue = typoographyFontWeightDict[transformedValue] || value;
+    }
+
+    return `$${name}-${toKebab(key)}: ${transformedValue}${separator}`
   }).join('\n');
 
   return `none;\n${stringValue}`;
